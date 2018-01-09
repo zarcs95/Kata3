55d04b0fbe2a6e5e86247e055c97a6b5ce9f570e
javi <javi@Desktop>
2018-01-09 17:05:05 +0000

    Kata3

---
+++ src/kata3/Kata3.java
@@ -0,0 +1,29 @@
+package kata3;
+
+public class Kata3 {
+
+    public static void main(String[] args) {
+        //HistogramDisplay histo = new HistogramDisplay();
+        //histo.execute();
+        Histogram<String> histogram = new Histogram();
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("hotmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("gmail.com");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        histogram.increment("ulpgc.es");
+        new HistogramDisplay(histogram).execute();
+    }
+    
+}
